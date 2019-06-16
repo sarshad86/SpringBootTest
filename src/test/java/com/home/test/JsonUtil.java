636841e3c8fe3bb3858936734b03d7548f21ff74
sArshad <sara.arshad.86@gmail.com>
2019-06-16 11:46:06 +0430

    edit jenkins file

--- src/test/java/com/home/test/JsonUtil.java
+++ src/test/java/com/home/test/JsonUtil.java
@@ -11,8 +11,10 @@ import java.io.IOException;
 public class JsonUtil {
 
     static byte[] toJson(Object object) throws IOException {
+
         ObjectMapper mapper = new ObjectMapper();
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         return mapper.writeValueAsBytes(object);
+
     }
 }
